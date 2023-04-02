80a3b434ab2287ca44b2012fb6e065918f61fbe6
Jelani Jackson <jelani.t.jackson@gmail.com>
2023-04-02 18:05:37 -0500

    Not using material UI

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,23 +1,9 @@
-import React from 'react';
 import Header from './components/Header';
-import User from './features/user/User';
-import Login from './components/authentication/Login';
-import Logout from './components/authentication/Logout';
-import { useAuth0 } from '@auth0/auth0-react';
 
 const App = () => {
-  const { isAuthenticated } = useAuth0();
-
-  const AuthButton = () => (isAuthenticated ? <Logout /> : <Login />);
-
   return (
     <>
-      <Header>
-        This is the header
-        <User />
-        <AuthButton />
-        All the way to here header
-      </Header>
+      <Header/>
       <main>This is the main content</main>
     </>
   );
--- web/src/components/Header/index.tsx
+++ web/src/components/Header/index.tsx
@@ -1,12 +1,21 @@
-import { AppBar } from '@mui/material';
-import { FC, ReactNode } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { FC } from 'react';
+import User from '../../features/user/User';
+import Login from '../authentication/Login';
+import Logout from '../authentication/Logout';
 
-interface HeaderProps {
-  children?: ReactNode;
-}
+const Header: FC = () => {
+  const { isAuthenticated } = useAuth0();
 
-const Header: FC<HeaderProps> = ({ children }) => {
-  return <AppBar position='sticky'>{children}</AppBar>;
+  const AuthButton = () => (isAuthenticated ? <Logout /> : <Login />);
+
+  return (
+    <header>
+      Magic is happening
+      <User />
+      <AuthButton />
+    </header>
+  );
 };
 
 export default Header;
