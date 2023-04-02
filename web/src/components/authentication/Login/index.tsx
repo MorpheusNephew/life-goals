ae05c51d8595b604b44d3015071d2dd262c2ddb1
Jelani Jackson <jelani.t.jackson@gmail.com>
2023-04-02 16:25:56 -0500

    Using loginWithRedirect

--- web/src/components/authentication/Login/index.tsx
+++ web/src/components/authentication/Login/index.tsx
@@ -1,7 +1,10 @@
+import { useAuth0 } from '@auth0/auth0-react';
 import { FC } from 'react';
 
 const Login: FC = () => {
-  return <div>Login</div>;
+  const { loginWithRedirect } = useAuth0();
+
+  return <button onClick={() => loginWithRedirect()}>Login</button>;
 };
 
 export default Login;
