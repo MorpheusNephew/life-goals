7e6de5a275a4451c3b991677d6106139e6a694e3
Jelani Jackson <jelani.t.jackson@gmail.com>
2023-04-02 16:26:08 -0500

    Using logout for logout

--- web/src/components/authentication/Logout/index.tsx
+++ web/src/components/authentication/Logout/index.tsx
@@ -1,7 +1,10 @@
+import { useAuth0 } from '@auth0/auth0-react';
 import { FC } from 'react';
 
 const Logout: FC = () => {
-  return <div>Logout</div>;
+  const { logout } = useAuth0();
+
+  return <button onClick={() => logout()}>Logout</button>;
 };
 
 export default Logout;
