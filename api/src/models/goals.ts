3386b9f49f675cde9c609ce81a7df5a54a78ad49
Jelani Jackson <jelani.jackson@lifeomic.com>
2023-04-09 12:55:18 -0500

    Added GoalsModel

---
+++ api/src/models/goals.ts
@@ -0,0 +1,59 @@
+import { randomUUID } from 'crypto';
+import { Goal, GoalDto, PostGoalDto, PutGoalDto } from '../types';
+
+export default class Goals {
+  private static list: Goal[] = [];
+
+  static createGoal(goalToCreate: PostGoalDto, creator: string) {
+    const createdGoal = new Goal({
+      ...goalToCreate,
+      createdDate: Date.now().toString(),
+      id: randomUUID(),
+      creator,
+    });
+
+    Goals.list.push(createdGoal);
+
+    return createdGoal;
+  }
+
+  static deleteGoal(goalId: string) {
+    Goals.list = Goals.list.filter((goal) => goal.id !== goalId);
+  }
+
+  static getAllGoals(publicGoals: boolean, creator?: string) {
+    return publicGoals
+      ? Goals.list.filter((goal) => goal.public)
+      : Goals.list.filter((goal) => goal.creator === creator);
+  }
+
+  static getGoal(goalId: string) {
+    return Goals.list.find((goal) => goal.id === goalId);
+  }
+
+  static updateGoal(goalId: string, updatedGoalInfo: PutGoalDto) {
+    const goalIndex = Goals.list.findIndex((goal) => goal.id === goalId);
+
+    if (goalIndex < 0) {
+      throw new Error('Goal not found');
+    }
+
+    const currentGoal = Goals.list[goalIndex];
+
+    const updatedGoal = new Goal({ ...currentGoal, ...updatedGoalInfo });
+
+    Goals.list[goalIndex] = updatedGoal;
+
+    return updatedGoal;
+  }
+
+  static toResource(goal: Goal): GoalDto {
+    return {
+      advice: goal.advice,
+      createdDate: goal.createdDate,
+      id: goal.id,
+      text: goal.text,
+      public: goal.public,
+    };
+  }
+}
