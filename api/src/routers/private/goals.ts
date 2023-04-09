94e4036e6c1a5b09ee0f90673fe3784c6ad19c9e
Jelani Jackson <jelani.jackson@lifeomic.com>
2023-04-09 12:56:34 -0500

    Implemented CRUD for private goals router

--- api/src/routers/private/goals.ts
+++ api/src/routers/private/goals.ts
@@ -1,7 +1,71 @@
-import express from 'express';
+import express, { Request } from 'express';
+import Goals from '../../models/goals';
+import { PostGoalDto, PutGoalDto } from '../../types';
 
-const privateGoalsRouter = express.Router().get('/', (_req, res) => {
-  res.send('Private Goals Router');
-});
+const privateGoalsRouter = express
+  .Router()
+  .get('/', (req, res) => {
+    res.json(
+      Goals.getAllGoals(false, req.auth?.payload.sub).map((goal) =>
+        goal.toResource()
+      )
+    );
+  })
+  .get('/:goalId', (req, res) => {
+    const currentUser = req.auth?.payload.sub!;
+    const goal = Goals.getGoal(req.params.goalId);
+
+    if (!goal) {
+      res.sendStatus(404);
+    } else if (goal.creator !== currentUser) {
+      res.sendStatus(403);
+    } else {
+      res.json(goal.toResource());
+    }
+  })
+  .post('/', (req: Request<{}, {}, PostGoalDto>, res) => {
+    const currentUser = req.auth?.payload.sub!;
+    const goalToCreate = req.body;
+
+    const createdGoal = Goals.createGoal(
+      goalToCreate,
+      currentUser
+    ).toResource();
+
+    res.json(createdGoal);
+  })
+  .put('/:goalId', (req: Request<{ goalId: string }, {}, PutGoalDto>, res) => {
+    const goalId = req.params.goalId;
+    const currentUser = req.auth?.payload.sub!;
+    const goal = Goals.getGoal(goalId);
+    const updatedGoalInfo = req.body;
+
+    if (!goal) {
+      res.sendStatus(404);
+    } else if (goal.creator !== currentUser) {
+      res.sendStatus(403);
+    } else {
+      const updatedGoal = Goals.updateGoal(
+        goalId,
+        updatedGoalInfo
+      ).toResource();
+
+      res.json(updatedGoal);
+    }
+  })
+  .delete('/:goalId', (req, res) => {
+    const goalId = req.params.goalId;
+    const currentUser = req.auth?.payload.sub!;
+    const goal = Goals.getGoal(goalId);
+
+    if (!goal) {
+      res.sendStatus(404);
+    } else if (goal.creator !== currentUser) {
+      res.sendStatus(403);
+    } else {
+      Goals.deleteGoal(goalId);
+      res.sendStatus(204);
+    }
+  });
 
 export default privateGoalsRouter;
