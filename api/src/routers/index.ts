28cdb3d6dfc4f9e1dca8f71217e028dd5999201c
Jelani Jackson <jelani.jackson@lifeomic.com>
2023-04-09 12:54:55 -0500

    Added body-parser

--- api/src/routers/index.ts
+++ api/src/routers/index.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import privateGoalsRouter from './private/goals';
 import publicRouter from './public';
 import privateRouter from './private';
+import bodyParser from 'body-parser';
 
 const routers = express
   .Router()
+  .use(bodyParser.json())
   .use('/public', publicRouter)
   .use('/private', privateRouter);
 
