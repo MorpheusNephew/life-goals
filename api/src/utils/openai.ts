6a8db34fea5652869676592584bf061d7907719b
Jelani Jackson <jelani.jackson@lifeomic.com>
2023-04-11 21:50:57 -0500

    Added open ai

---
+++ api/src/utils/openai.ts
@@ -0,0 +1,15 @@
+import { Configuration, OpenAIApi } from 'openai';
+import { OPENAI_API_KEY, OPENAI_ORGANIZATION_ID } from './environmentVariables';
+
+const configuration = new Configuration({
+  organization: OPENAI_ORGANIZATION_ID,
+  apiKey: OPENAI_API_KEY,
+});
+
+const openAI = new OpenAIApi(configuration);
+
+export const getGoalAdvice = () => {
+  console.log({ openAI });
+};
+
+export default openAI;
