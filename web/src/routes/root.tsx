c9688548966ba1b3ad292f48d0769c69168c90ad
Jelani Jackson <jelani.jackson@lifeomic.com>
2023-04-12 20:40:11 -0500

    Updated styling

--- web/src/routes/root.tsx
+++ web/src/routes/root.tsx
@@ -1,22 +1,31 @@
 import Header from '../components/header';
 import { Outlet } from 'react-router-dom';
-import { Box, Drawer, Toolbar } from '@mui/material';
+import { AppBar, Box, Drawer, Toolbar } from '@mui/material';
 
 const Root = () => {
   return (
     <Box sx={{ display: 'flex' }}>
-      <Header />
+      <AppBar
+        position="fixed"
+        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      >
+        More magic
+      </AppBar>
       <Drawer
         variant="permanent"
         sx={{
           width: 240,
           flexShrink: 0,
-          [`& .MuiDrawer-paper`]: { width: 240, boxSizing: 'border-box' },
+          [`& .MuiDrawer-paper`]: {
+            width: 240,
+            boxSizing: 'border-box',
+            marginTop: 5,
+          },
         }}
       >
-        Hello
+        <Header />
       </Drawer>
-      <Box component={'main'} sx={{ flexGrow: 1, p: 3 }}>
+      <Box component={'main'} sx={{ flexGrow: 1, p: 5, marginTop: -8 }}>
         <Toolbar />
         <Outlet />
       </Box>
