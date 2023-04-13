5a6045037c42736fed05414e05d2fe99099786c3
Jelani Jackson <jelani.jackson@lifeomic.com>
2023-04-12 20:41:03 -0500

    Added react-intl messages

--- web/src/components/authentication/Login/index.tsx
+++ web/src/components/authentication/Login/index.tsx
@@ -1,10 +1,24 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import { FC } from 'react';
+import { defineMessages, useIntl } from 'react-intl';
+
+const messages = defineMessages({
+  loginButton: {
+    id: 'app.login.loginButton',
+    defaultMessage: 'Login',
+    description: 'Button used to log into Life Goals',
+  },
+});
 
 const Login: FC = () => {
   const { loginWithRedirect } = useAuth0();
+  const { formatMessage } = useIntl();
 
-  return <button onClick={() => loginWithRedirect()}>Login</button>;
+  return (
+    <button onClick={() => loginWithRedirect()}>
+      {formatMessage(messages.loginButton)}
+    </button>
+  );
 };
 
 export default Login;
--- web/src/components/authentication/Logout/index.tsx
+++ web/src/components/authentication/Logout/index.tsx
@@ -1,10 +1,24 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import { FC } from 'react';
+import { defineMessages, useIntl } from 'react-intl';
+
+const messages = defineMessages({
+  logoutButton: {
+    id: 'app.logout.logoutButton',
+    defaultMessage: 'Logout',
+    description: 'Button used to log out of Life Goals',
+  },
+});
 
 const Logout: FC = () => {
   const { logout } = useAuth0();
+  const { formatMessage } = useIntl();
 
-  return <button onClick={() => logout()}>Logout</button>;
+  return (
+    <button onClick={() => logout()}>
+      {formatMessage(messages.logoutButton)}
+    </button>
+  );
 };
 
 export default Logout;
