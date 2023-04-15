ce4924b80cb98aba01c8e71fd7b7b404445bcf3e
Jelani Jackson <jelani.t.jackson@gmail.com>
2023-04-14 20:43:38 -0500

    Added New Goal button

--- web/src/routes/goals.tsx
+++ web/src/routes/goals.tsx
@@ -4,6 +4,8 @@ import { defineMessages, useIntl } from 'react-intl';
 import Loading from '../components/loading';
 import { getUserGoals } from '../services/api';
 import { GoalDto } from '../services/api/types';
+import { Box, Button } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const messages = defineMessages({
   personalGoalsHeader: {
@@ -14,9 +16,13 @@ const messages = defineMessages({
     id: 'app.goals.noPersonalGoals',
     defaultMessage: "You don't have any goals",
   },
+  newGoalButtonText: {
+    id: 'app.goals.newGoalButtonText',
+    defaultMessage: 'New goal',
+  },
 });
 
-const showGoal = (goal: any) => <p>{goal.text}</p>;
+const showGoal = (goal: any) => <p key={goal.id}>{goal.text}</p>;
 
 const Goals = () => {
   const [goals, setGoals] = useState<GoalDto[]>();
@@ -39,7 +45,14 @@ const Goals = () => {
   }, [getAccessTokenSilently, setIsLoading]);
   return (
     <section>
-      <h2>{formatMessage(messages.personalGoalsHeader)}</h2>
+      <Box>
+        <h2>{formatMessage(messages.personalGoalsHeader)}</h2>
+        <Link to={'/goals/new'}>
+          <Button variant='outlined'>
+            {formatMessage(messages.newGoalButtonText)}
+          </Button>
+        </Link>
+      </Box>
       {isLoading ? (
         <Loading />
       ) : goals && goals.length > 0 ? (
