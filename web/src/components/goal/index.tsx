3508ad7d9317290916a7637ea250190908ff0f3a
Jelani Jackson <jelani.t.jackson@gmail.com>
2023-04-21 20:57:43 -0500

    Added header text

--- web/src/components/goal/index.tsx
+++ web/src/components/goal/index.tsx
@@ -1,21 +1,35 @@
 import { FC } from 'react';
 import { GoalDto } from '../../services/api/types';
 import { Box, Typography } from '@mui/material';
-import { useIntl } from 'react-intl';
+import { defineMessages, useIntl } from 'react-intl';
+
+const messages = defineMessages({
+  prompt: {
+    id: 'app.goalview.prompt',
+    defaultMessage: 'Prompt:'
+  },
+  advice: {
+    id: 'app.goalView.advice',
+    defaultMessage: 'Advice:'
+  }
+});
 
 interface GoalViewProps {
   goal: GoalDto;
 }
 
 const GoalView: FC<GoalViewProps> = ({ goal }) => {
-  const { formatDate } = useIntl();
+  const { formatDate, formatMessage } = useIntl();
 
   return (
-    <Box sx={{border: 1, margin: 1, padding: 1}}>
+    <Box sx={{ border: 1, margin: 1, padding: 1 }}>
       <Typography>
-        {goal.text} - {formatDate(goal.createdDate)}
+        <strong>{formatMessage(messages.prompt)}</strong> {goal.text} -{' '}
+        {formatDate(goal.createdDate)}
       </Typography>
-      <Typography>{goal.advice}</Typography>
+      <Typography>
+       <strong>{formatMessage(messages.advice)}</strong> {goal.advice}
+      </Typography>
     </Box>
   );
 };
