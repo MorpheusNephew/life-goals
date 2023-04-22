fea093249e3c5b09f76c5dd580feb861f8a882cb
Jelani Jackson <jelani.t.jackson@gmail.com>
2023-04-22 00:06:30 -0500

    Added attributes

--- api/terraform/dynamodb.tf
+++ api/terraform/dynamodb.tf
@@ -1,3 +1,27 @@
 resource "aws_dynamodb_table" "life_goals_db" {
-  name = "life-goals-db"
+  name         = "life-goals-db"
+  hash_key     = "userId"
+  range_key    = "createdDate"
+  billing_mode = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "userId"
+    type = "S"
+  }
+
+  attribute {
+    name = "createdDate"
+    type = "S"
+  }
+
+  attribute {
+    name = "public"
+    type = "BOOL"
+  }
+
+  global_secondary_index {
+    name = "PublicCreatedDateIndex"
+    hash_key = "pubic"
+    range_key = "createdDate"
+  }
 }
