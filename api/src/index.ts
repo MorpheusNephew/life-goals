d67e0ee77848607edb7322c3d09584010ef302cd
Jelani Jackson <jelani.t.jackson@gmail.com>
2023-04-22 17:25:44 -0500

    Converted index to commonjs

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,17 +1,23 @@
 import express from 'express';
 import routers from './routers';
 import { initializeDb } from './utils/db';
-
-const app = express();
-const port = process.env.PORT ?? 3001;
-await initializeDb();
-
-app.use('/api', routers);
+import { LOCAL_DEV } from './utils/variables';
 
 export const handler = () => {
   console.log("What's up");
 };
 
-app.listen(port, async () => {
-  console.log(`Listening on http://localhost:${port}`);
-});
+const app = express();
+const port = process.env.PORT ?? 3001;
+
+app.use('/api', routers);
+
+(async () => {
+  await initializeDb();
+
+  if (LOCAL_DEV) {
+    app.listen(port, async () => {
+      console.log(`Listening on http://localhost:${port}`);
+    });
+  }
+})();
